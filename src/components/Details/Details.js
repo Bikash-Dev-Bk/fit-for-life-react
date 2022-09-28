9914977970ef3271ad4acc6405d3b0a833361d16
bikash-dev <bikash.dev.bk@gmail.com>
2022-09-29 03:32:48 +0600

    exercise details added

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -4,6 +4,32 @@ import './Details.css'
 const Details = () => {
     return (
         <div className='details-container'>
+            <div className='my-info'>
+                <h3>Bikash Devsharma</h3>
+                <p>Junior Web Developer</p>
+                <p>Age: 25 years</p>
+                <p>Height: 5.8 feet</p>
+                <p>Weight: 70 KG</p>
+            </div>
+            
+            <div className='break-container'>
+                <h2>Add A Break</h2>
+                <div className='break'>
+                    <p>10s</p>
+                    <p>20s</p>
+                    <p>30s</p>
+                    <p>40s</p>
+                    <p>50s</p>
+                </div>
+            </div>
+            
+            <div className='exercise-details'>
+                <h2>Exercise Details</h2>
+                <p className='exercise-details-time'>Exercise time</p>
+                <p className='exercise-details-time'>Break time</p>
+            </div>
+
+            <button><p>Activity Completed</p></button>
             
         </div>
     );
